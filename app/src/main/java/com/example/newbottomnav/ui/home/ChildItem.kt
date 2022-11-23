28e4d0a68862b2ce8dcdeefe2a1054f358ea2aff
Nellllyaa <amandollaneila.2003@gmail.com>
2022-11-24 02:32:56 +0600

    Added new class ChildItem and given a variable

---
+++ app/src/main/java/com/example/newbottomnav/ui/home/ChildItem.kt
@@ -0,0 +1,6 @@
+package com.example.newbottomnav.ui.home
+
+class ChildItem(var itemName: String,
+                var itemQty: String,
+                var itemPrice: String,
+                var imageID: Int)
