73e6cdce62d6d014dbd630411ceda299a531a73a
Nellllyaa <amandollaneila.2003@gmail.com>
2022-11-24 03:14:51 +0600

    Class comments are wrong

--- app/src/main/java/com/example/newbottomnav/ui/home/ChildItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newbottomnav.ui.home
-
-class ChildItem(var itemName: String,
-                var itemQty: String,
-                var itemPrice: String,
-                var imageID: Int)
--- app/src/main/java/com/example/newbottomnav/ui/home/ParentItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newbottomnav.ui.home
-
-class ParentItem(
-    var orderId: String,
-    var quantity: String,
-    var price: String,
-    var imageId: Int)
