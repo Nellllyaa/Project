f03b7f7008428438e87e400f39f1d31694971bad
Nellllyaa <amandollaneila.2003@gmail.com>
2022-12-05 00:43:17 +0600

    Added function CardView and Recycler view , also added other drawable

--- app/src/main/java/com/example/newbottomnav/HomeFragment.kt
+++ app/src/main/java/com/example/newbottomnav/HomeFragment.kt
@@ -1,22 +1,25 @@
 package com.example.newbottomnav
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.example.newbottomnav.databinding.FragmentHomeBinding
-import java.util.zip.Inflater
+import com.example.newbottomnav.pojo.Meal
+import com.example.newbottomnav.videoModel.HomeViewModel
+
+
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var homeMvvm: HomeViewModel
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
+
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +30,42 @@ class HomeFragment : Fragment() {
         return root
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        homeMvvm.getRandomMeal()
+        observerRandomMeal()
+
+
+
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        homeMvvm = ViewModelProvider(this)[HomeViewModel::class.java]
+    }
+    private fun observerRandomMeal() {
+        homeMvvm.observeRandomMealLivedata().observe(viewLifecycleOwner
+        ) { t -> Glide.with(context).load(t!!.strMealThumb).into(binding.randomMeal) }
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/newbottomnav/MainActivity.kt
+++ app/src/main/java/com/example/newbottomnav/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.newbottomnav
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,11 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+//        replaceFragment(HomeFragment())
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -34,4 +33,13 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    private fun replaceFragment(homeFragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,homeFragment)
+        fragmentTransaction.commit()
+    }
+
+
 }
